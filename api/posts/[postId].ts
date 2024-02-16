2adbcfe153b44dadb8470c963f44041c8069a5b6
David <112674521+DavidxDl@users.noreply.github.com>
2024-02-16 13:48:34 -0300

    Update [postId].ts

--- api/posts/[postId].ts
+++ api/posts/[postId].ts
@@ -7,21 +7,28 @@ export default async function handler(
 ) {
   console.log(req.body);
   try {
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), 10000); // Abort after 10 seconds
+
     const apiResponse = await fetch(
       `http://172.233.16.85/posts/${req.query.postId}`,
       {
         method: "PATCH",
         headers: {
-          accept: "application/json",
           "Content-Type": "application/json",
         },
         body: JSON.stringify(req.body),
-        timeout: 10000,
+        signal: controller.signal,
       },
     );
-    if (!apiResponse.ok)
-      throw new Error(`error Patching /posts/${req.query.postId} body: ${req.body}`);
-    res.status(200);
+
+    clearTimeout(timeoutId); // Clear the timeout if the request completes within the timeout duration
+
+    if (!apiResponse.ok) {
+      throw new Error(`Error Patching /posts/${req.query.postId}, Status: ${apiResponse.status}`);
+    }
+
+    res.status(200).json({ success: true }); // Send a response indicating success
   } catch (error) {
     console.error("Error fetching data from API:", error);
     res.status(500).json({ error: "Failed to fetch data from API" });
