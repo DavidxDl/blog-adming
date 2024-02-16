3b064430c59c6dfb793fdf7504f49e4c530d0c87
DavidxDl <Davidbekamlopez@gmail.com>
2024-02-16 13:05:58 -0300

    proxy commit

--- api/posts/[postId].ts
+++ api/posts/[postId].ts
@@ -5,7 +5,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  console.log(req.body.published);
+  console.log(req.body);
   try {
     const apiResponse = await fetch(
       `http://172.233.16.85/posts/${req.query.postId}`,
@@ -19,7 +19,7 @@ export default async function handler(
       },
     );
     if (!apiResponse.ok)
-      throw new Error(`error Patching /posts/${req.query.postId}`);
+      throw new Error(`error Patching /posts/${req.query.postId} body: ${req.body}`);
     res.status(200);
   } catch (error) {
     console.error("Error fetching data from API:", error);
