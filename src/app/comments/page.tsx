b9cffc40045013eb4f15b3b5b3965a33f5581df7
DavidxDl <Davidbekamlopez@gmail.com>
2024-02-17 15:44:50 -0300

    proxy commit

---
+++ src/app/comments/page.tsx
@@ -0,0 +1,30 @@
+interface comment {
+  email: string;
+  text: string;
+  timestamp: string;
+  id: string;
+}
+export default async function Comments() {
+  const comments: comment[] = await getComments();
+  return (
+    <>
+      <h1 className="text-5xl font-extrabold">Posts</h1>
+      {comments ? (
+        comments.map((c) => (
+          <div key={c.id}>
+            <h2>{c.email}</h2>
+            <p>{c.text}</p>
+          </div>
+        ))
+      ) : (
+        <p>no comments</p>
+      )}
+    </>
+  );
+}
+
+async function getComments():Promise<comment[]> {
+  const res = await fetch("/api/comments");
+  const data: comment[] =  await res.json() as comment[];
+  return data;
+}
