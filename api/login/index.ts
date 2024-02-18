6b4a41b775d075d57a22d931d04134fab63248fc
DavidxDl <Davidbekamlopez@gmail.com>
2024-02-18 16:26:33 -0300

    proxy commit

---
+++ api/login/index.ts
@@ -0,0 +1,30 @@
+/* eslint-disable */
+
+import { NextApiRequest, NextApiResponse } from "next";
+
+interface data {
+  acessToken: string;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  try {
+    const apiRes = await fetch("http://172.233.16.85/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: req.body.username, password: req.body.password }),
+    });
+    if (!apiRes.ok) {
+      throw new Error("Wrong credentials");
+    }
+    const data: data = (await apiRes.json()) as data;
+    res.status(200).json({token: data.acessToken})
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
